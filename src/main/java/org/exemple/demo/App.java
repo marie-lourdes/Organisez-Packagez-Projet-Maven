14657b5caed81473ffadaa1d1357191262957fe0
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-08-23 10:15:03 +0200

    App.java: add code to display property created org.exemple.demo.version

--- src/main/java/org/exemple/demo/App.java
+++ src/main/java/org/exemple/demo/App.java
@@ -1,13 +1,30 @@
 package org.exemple.demo;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World!");
+		Properties vProp = new Properties();
+		InputStream vInputStream = null;
+
+		try {
+			// getResourceAsStream, methode des type class
+			vInputStream = App.class.getResourceAsStream("info.properties");
+			// load() methode de la classe Properties, qui une liste de proprité dans un
+			// inputStream
+			vProp.load(vInputStream);
+			System.out.println(" propriété de version de l application defini dans info.properties: "
+					+ vProp.getProperty(org.exemple.demo.version, "?"));
+			// on recupere la propriété crée dans info.properties , dont la valeur est passé
+			// dans le second parametre avec "?"
+		} finally {
+
+		}
+	}
 }
